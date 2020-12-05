b11c374125401c80f1a8af37e876101839ac64ff
Chris Connelly <github@logon.pw>
2020-12-05 01:36:11 -0500

    made bit size independent

--- aoc5/main.zig
+++ aoc5/main.zig
@@ -1,28 +1,31 @@
 const std = @import("std");
 const input = @embedFile("input");
+const BITS = 10;
+const uBITS = u10;
+const maxsize = std.math.maxInt(uBITS);
 
-pub fn seatID(pass: []const u8) !u16 {
-    var buf: [10]u8 = undefined;
+pub fn seatID(pass: []const u8) !uBITS {
+    var buf: [BITS]u8 = undefined;
     _ = std.mem.replace(u8, pass[0..10], "F", "0", &buf);
     _ = std.mem.replace(u8, buf[0..], "B", "1", &buf);
     _ = std.mem.replace(u8, buf[0..], "L", "0", &buf);
     _ = std.mem.replace(u8, buf[0..], "R", "1", &buf);
-    return std.fmt.parseUnsigned(u16, buf[0..], 2);
+    return std.fmt.parseUnsigned(uBITS, buf[0..], 2);
 }
 
 pub fn main() anyerror!void {
-    var maxID: u16 = 0;
-    var minID: u16 = 1024;
-    var seats: [1024]bool = std.mem.zeroes([1024]bool);
+    var maxID: uBITS = 0;
+    var minID: uBITS = maxsize;
+    var seats: [maxsize]bool = std.mem.zeroes([maxsize]bool);
     var it = std.mem.split(input, "\n");
     while (it.next()) |line| {
-        if (line.len != 10) continue;
+        if (line.len != BITS) continue;
         const seat = try seatID(line);
         seats[seat] = true;
         minID = std.math.min(minID, seat);
         maxID = std.math.max(maxID, seat);
     }
-    var missingSeat: u16 = 1025;
+    var missingSeat: uBITS = maxsize;
     var i = minID;
     while (i < maxID) : (i += 1) {
         if (!seats[i]) missingSeat = i;
