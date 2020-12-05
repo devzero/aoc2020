02c6e232f74c52b72f33ddfd378e36f4b91881f0
Chris Connelly <github@logon.pw>
2020-12-05 00:45:13 -0500

    solved day 5

---
+++ aoc5/main.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const input = @embedFile("input");
+
+pub fn seatID(pass: []const u8) !u16 {
+    var buf: [10]u8 = undefined;
+    var num_replaced = std.mem.replace(u8, pass[0..], "F", "0", &buf);
+    num_replaced = std.mem.replace(u8, buf[0..], "B", "1", &buf);
+    num_replaced = std.mem.replace(u8, buf[0..], "L", "0", &buf);
+    num_replaced = std.mem.replace(u8, buf[0..], "R", "1", &buf);
+    return std.fmt.parseUnsigned(u16, buf[0..10], 2);
+}
+
+pub fn main() anyerror!void {
+    var maxID: u16 = 0;
+    var minID: u16 = 1024;
+    var seats: [1024]bool = std.mem.zeroes([1024]bool);
+    var it = std.mem.split(input, "\n");
+    while (it.next()) |line| {
+        if (line.len != 10) continue;
+        const seat = try seatID(line);
+        seats[seat] = true;
+        minID = std.math.min(minID, seat);
+        maxID = std.math.max(maxID, seat);
+    }
+    var missingSeat: u16 = 1024;
+    var i = minID;
+    while (i < maxID) : (i += 1) {
+        if (!seats[i]) missingSeat = i;
+    }
+    std.log.info("part1: {}", .{maxID});
+    std.log.info("part2: {}", .{missingSeat});
+}
