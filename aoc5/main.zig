7913624f55ca7e1eec0b4b2b15f908bb400d7198
Chris Connelly <github@logon.pw>
2020-12-05 00:49:49 -0500

    more cleanup

--- aoc5/main.zig
+++ aoc5/main.zig
@@ -3,11 +3,11 @@ const input = @embedFile("input");
 
 pub fn seatID(pass: []const u8) !u16 {
     var buf: [10]u8 = undefined;
-    var num_replaced = std.mem.replace(u8, pass[0..10], "F", "0", &buf);
-    num_replaced = std.mem.replace(u8, buf[0..], "B", "1", &buf);
-    num_replaced = std.mem.replace(u8, buf[0..], "L", "0", &buf);
-    num_replaced = std.mem.replace(u8, buf[0..], "R", "1", &buf);
-    return std.fmt.parseUnsigned(u16, buf[0..10], 2);
+    _ = std.mem.replace(u8, pass[0..10], "F", "0", &buf);
+    _ = std.mem.replace(u8, buf[0..], "B", "1", &buf);
+    _ = std.mem.replace(u8, buf[0..], "L", "0", &buf);
+    _ = std.mem.replace(u8, buf[0..], "R", "1", &buf);
+    return std.fmt.parseUnsigned(u16, buf[0..], 2);
 }
 
 pub fn main() anyerror!void {
@@ -22,7 +22,7 @@ pub fn main() anyerror!void {
         minID = std.math.min(minID, seat);
         maxID = std.math.max(maxID, seat);
     }
-    var missingSeat: u16 = 1024;
+    var missingSeat: u16 = 1025;
     var i = minID;
     while (i < maxID) : (i += 1) {
         if (!seats[i]) missingSeat = i;
