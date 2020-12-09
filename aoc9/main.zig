3d2fb140420f22a2359123b36ba1cf9a54407893
Chris Connelly <github@logon.pw>
2020-12-09 01:26:26 -0500

    cleaned up code

--- aoc9/main.zig
+++ aoc9/main.zig
@@ -3,8 +3,36 @@ const input = @embedFile("input");
 const WIN = 25;
 const SUMSSIZE = (WIN * WIN - WIN) / 2;
 const Allocator = std.mem.Allocator;
-const HashMap = std.AutoHashMap;
-const ArrayList = std.ArrayList;
+
+pub fn parseInput(inp: []const u8, allocator: *Allocator) ![]u64 {
+    var lines = std.mem.split(inp, "\n");
+    var numsAL = std.ArrayList(u64).init(std.heap.page_allocator);
+    defer numsAL.deinit();
+    while (lines.next()) |line| {
+        if (line.len == 0) continue;
+        try numsAL.append(try std.fmt.parseUnsigned(u64, line, 10));
+    }
+    return numsAL.toOwnedSlice();
+}
+
+pub fn solvePart1(nums: []u64) !u64 {
+    var cur: u16 = WIN;
+    outer: while (cur < nums.len) : (cur += 1) {
+        var i: u16 = 0;
+        var j: u16 = 0;
+        while (i < WIN) : (i += 1) {
+            j = i + 1;
+            while (j < WIN) : (j += 1) {
+                const thissum = nums[cur + i - WIN] + nums[cur + j - WIN];
+                if (nums[cur] == thissum) {
+                    continue :outer;
+                }
+            }
+        }
+        return nums[cur];
+    }
+    return error.noAnswer;
+}
 
 pub fn minNum(s: []u64) u64 {
     var a: u64 = std.math.maxInt(u64);
@@ -20,51 +48,29 @@ pub fn maxNum(s: []u64) u64 {
     return a;
 }
 
-pub fn main() anyerror!void {
-    var allocator = std.heap.page_allocator;
-    var lines = std.mem.split(input, "\n");
-    var numsAL = ArrayList(u64).init(allocator);
-    defer numsAL.deinit();
-    while (lines.next()) |line| {
-        if (line.len == 0) continue;
-        try numsAL.append(try std.fmt.parseUnsigned(u64, line, 10));
-    }
-    const nums = numsAL.toOwnedSlice();
-
-    var cur: u16 = WIN;
-    var part1: u64 = 0;
-    outer: while (cur < nums.len) : (cur += 1) {
-        var i: u16 = 0;
-        var j: u16 = 0;
-        while (i < WIN) : (i += 1) {
-            j = i + 1;
-            while (j < WIN) : (j += 1) {
-                const thissum = nums[cur + i - WIN] + nums[cur + j - WIN];
-                if (nums[cur] == thissum) {
-                    continue :outer;
-                }
-            }
-        }
-        part1 = nums[cur];
-        break :outer;
-    }
-
-    var part2: u64 = 0;
-    outer2: for (nums) |_, size| {
+pub fn solvePart2(nums: []u64, part1: u64) !u64 {
+    for (nums) |_, size| {
         if (size < 2) continue;
-        cur = 0;
+        var cur: u16 = 0;
         while (cur < (nums.len - size)) : (cur += 1) {
             var sum: u64 = 0;
             var i: usize = 0;
             while (i < size) : (i += 1) {
                 sum += nums[cur + i];
             }
-            if (sum == part1) {
-                part2 = minNum(nums[cur .. cur + size - 1]) + maxNum(nums[cur .. cur + size - 1]);
-                break :outer2;
-            }
+            if (sum == part1)
+                return minNum(nums[cur .. cur + size - 1]) + maxNum(nums[cur .. cur + size - 1]);
         }
     }
+    return error.noAnswer;
+}
+
+pub fn main() anyerror!void {
+    const nums = try parseInput(input, std.heap.page_allocator);
+    defer std.heap.page_allocator.free(nums);
+
+    const part1 = try solvePart1(nums);
+    const part2 = try solvePart2(nums, part1);
 
     std.log.info("Part1: {}", .{part1});
     std.log.info("Part2: {}", .{part2});
